d9d3b7b86e20364444742162b6710390009532f6
Kerby Jean <Kurbs@Kerbys-MacBook-Pro.local>
2020-07-17 11:59:12 -0400

    improved runtime by 6 seconds

--- names/names.py
+++ names/names.py
@@ -1,22 +1,29 @@
 import time
+from collections import Counter
+
+
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('/Users/Kurbs/Desktop/CS/Sprint-Challenge--Data-Structures-Python/names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('/Users/Kurbs/Desktop/CS/Sprint-Challenge--Data-Structures-Python/names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# dups = [k for k,v in Counter(names_1).items() if v>1]
+# duplicates = dups
+
+dups = [x for x in names_1 if names_1.count(x) >= 2]
+duplicates = dups
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
