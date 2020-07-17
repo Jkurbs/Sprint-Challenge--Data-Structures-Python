11f4ec172bdbe35a45bdcb0ad339a9eff0525d0e
Kerby Jean <Kurbs@Kerbys-MacBook-Pro.local>
2020-07-17 13:32:19 -0400

    Not complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,59 @@
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+
 class RingBuffer:
     def __init__(self, capacity):
+        self.capacity = capacity 
+        self.size = 0 
+        self.head = None
         pass
 
     def append(self, item):
-        pass
+        self.size += 1 
+        current_node = self.head
+        new_node = Node(item)
+
+        # Check for capacity 
+        if self.size > self.capacity: 
+            print("Full")
+            while current_node.next_node: 
+                current_node = current_node.next_node 
+                print("Node:", current_node)
+            new_node.next_node = self.head.next_node
+            self.head = None
+            current_node.next_node = new_node
+            new_node = self.head  
+
+        # Check if it's empty 
+        elif not self.head:
+            self.head = new_node
+            new_node.next_node = self.head
+        else: 
+            while current_node.next_node != self.head: 
+                current_node = current_node.next_node 
+            current_node.next_node = new_node 
+            new_node.next_node = self.head
+            
+
+
 
     def get(self):
-        pass
+        current_node = self.head 
+        while current_node: 
+            print(current_node.value)
+            current_node = current_node.next_node
+            if current_node == self.head: 
+                break
+
+ring = RingBuffer(3) 
+
+ring.append(0)
+ring.append(1)
+ring.append(2)
+ring.append(3)
+
+ring.get()
